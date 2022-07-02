afb6f6a0821977a06c150997c0884d54a2599745
Klop233 <3337913379@qq.com>
2022-07-02 19:23:23 +0800

    添加绑定和执行命令功能

--- src/main/java/me/klop233/Noir/Command.java
+++ src/main/java/me/klop233/Noir/Command.java
@@ -19,27 +19,33 @@ public class Command implements CommandExecutor {
         }
 
         if (args[0].equalsIgnoreCase("help")) {
-            sender.sendMessage("--------------[ Noir ]--------------");
+            sender.sendMessage(ChatColor.LIGHT_PURPLE + "-------------[ Noir ]-------------");
+            sender.sendMessage("/noir help        显示本帮助信息");
             sender.sendMessage("/noir reload      重载插件");
             sender.sendMessage("/noir version     查看插件信息");
+            sender.sendMessage(ChatColor.LIGHT_PURPLE + "----------[ By " + ChatColor.AQUA + "Klop233 " + ChatColor.LIGHT_PURPLE+ "]----------");
         } else if (args[0].equalsIgnoreCase("reload")) {
             long t1 = System.currentTimeMillis();
             Noir.reloadNoir();
             long t2 = System.currentTimeMillis();
-            sender.sendMessage(ChatColor.LIGHT_PURPLE + "Noir配置文件重载成功 [" + (t2-t1) + "ms]");
+            sender.sendMessage(ChatColor.LIGHT_PURPLE + "Noir 配置文件重载成功 [" + (t2-t1) + "ms]");
 
 
         } else if (args[0].equalsIgnoreCase("version")) {
-            sender.sendMessage("--------[ 插件信息 ]----------");
+            sender.sendMessage(ChatColor.LIGHT_PURPLE + "--------[ 插件信息 ]----------");
             sender.sendMessage(ChatColor.LIGHT_PURPLE + "Noir -> " + Noir.version);
             sender.sendMessage(ChatColor.GREEN + "MiraiMC -> " + Noir.miraiVersion);
             sender.sendMessage("QQ Bot号: " + Data.botsID);
             sender.sendMessage("QQ 群号: " + Data.groupsID);
-            sender.sendMessage("------------------------------");
+            if (Noir.init) {
+                sender.sendMessage(ChatColor.LIGHT_PURPLE + "Noir " + ChatColor.GREEN +"已就绪");
+            } else {
+                sender.sendMessage(ChatColor.LIGHT_PURPLE + "Noir " + ChatColor.RED + "未准备好");
+            }
+            sender.sendMessage(ChatColor.LIGHT_PURPLE + "--------[ By " + ChatColor.AQUA + "Klop233 " + ChatColor.LIGHT_PURPLE+ "]--------");
         } else {
             sender.sendMessage(ChatColor.LIGHT_PURPLE + "Noir " + Noir.version + ChatColor.GREEN + " 正在这个服务器上运行, 使用 /noir help 来获取帮助");
         }
-
 
         return true;
     }
--- src/main/java/me/klop233/Noir/Events/GroupMessage.java
+++ src/main/java/me/klop233/Noir/Events/GroupMessage.java
@@ -1,21 +1,30 @@
 package me.klop233.Noir.Events;
 
 
+import me.dreamvoid.miraimc.api.MiraiMC;
 import me.dreamvoid.miraimc.bukkit.event.message.passive.MiraiGroupMessageEvent;
+import me.klop233.Noir.Commander;
 import me.klop233.Noir.Data;
 import me.klop233.Noir.Noir;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
 
 
 public class GroupMessage implements Listener {
     @EventHandler
     public void list(MiraiGroupMessageEvent e) {
         if (!(Data.botsID == e.getBotID()) ||
-            !e.getMessage().replace(" ","").startsWith(Data.list) ||
-            !Noir.init) {
-            return ;
+                !e.getMessage().replace(" ", "").startsWith(Data.list) ||
+                !Noir.init) {
+            return;
         }
 
 
@@ -37,6 +46,89 @@ public class GroupMessage implements Listener {
 
     @EventHandler
     public void bind(MiraiGroupMessageEvent e) {
+        if (!(Data.botsID == e.getBotID()) ||
+                !e.getMessage().replace(" ", "").startsWith(Data.bind) ||
+                !Noir.init) {
+            return;
+        }
 
+        String name = e.getMessage().replace(" ", "").substring(Data.bind.length());
+        long bindQQID = e.getSenderID();
+        if ("".equals(name)) {
+            e.sendMessage("用法：#绑定 [ID]");
+        } else if (name.length() > 25) {
+            e.sendMessage("你的ID长度超过限制！[25]");
+        } else {
+            if (MiraiMC.getBinding(name) != 0L) {
+                e.getGroup().sendMessage("绑定失败，你要绑定的ID已经绑定了一个QQ: " + Objects.requireNonNull(Bukkit.getPlayer(UUID.fromString(MiraiMC.getBinding(bindQQID)))).getDisplayName() + "，请更换你的游戏ID");
+            } else {
+                if (!MiraiMC.getBinding(bindQQID).equals("")) {
+                    e.getGroup().sendMessage("已替换绑定[" + name + "]！请勿频繁替换绑定！");
+                } else {
+                    e.getGroup().sendMessage("绑定成功[" + name + "]，白名单已自动添加！");
+                }
+
+                MiraiMC.addBinding(Bukkit.getOfflinePlayer(name).getUniqueId().toString(), bindQQID);
+
+                Commander send = new Commander();
+
+                try {
+                    for (String cmd : Noir.config.getStringList("qq.commands-after-bind")) {
+                        Bukkit.getScheduler().callSyncMethod(Noir.INSTANCE, () -> Bukkit.dispatchCommand(send, cmd.replace("[player-name]", name))).get();
+                    }
+                } catch (ExecutionException | InterruptedException ex) {
+                    Noir.info("Noir 执行命令时出错, 但是这一错误一般无需处理");
+                    e.sendMessage("执行命令出错");
+                }
+
+//                if (send.message.size() == 0) {
+//                    e.sendMessage("已执行, 命令无返回");
+//                    return ;
+//                }
+//
+//                StringBuilder reply = new StringBuilder();
+//                for (String s : send.message) {
+//                    reply.append(s).append("\n");
+//                }
+//
+//                e.sendMessage(reply.toString());
+            }
+        }
+    }
+
+    @EventHandler
+    public void execute(MiraiGroupMessageEvent e) {
+        if (!(Data.botsID == e.getBotID()) ||
+                !e.getMessage().replace(" ", "").startsWith(Data.execute) ||
+                !Noir.init) {
+            return;
+        }
+        if (!Data.adminsID.contains(e.getSenderID())) {
+            e.sendMessage("你没有权限");
+            return ;
+        }
+
+        Commander send = new Commander();
+
+        try {
+            Bukkit.getScheduler().callSyncMethod(Noir.INSTANCE, () -> Bukkit.dispatchCommand(send, e.getMessage().substring(Data.execute.length()))).get();
+        } catch (ExecutionException | InterruptedException var6) {
+            Noir.info("Noir 执行命令时出错, 但是这一错误一般无需处理");
+            e.sendMessage("执行命令出错");
+            return ;
+        }
+
+        if (send.message.size() == 0) {
+            e.getGroup().sendMessage("已执行，命令无返回");
+        } else {
+            StringBuilder groupReply = new StringBuilder();
+
+            for (String s : send.message) {
+                groupReply.append(s).append("\n");
+            }
+
+            groupReply.delete(groupReply.length() - 1, groupReply.length());
+            e.getGroup().sendMessage(ChatColor.stripColor(groupReply.toString()));
+        }
     }
 }
